dd4bc10f202bfccb76aae598795222a09399a7f9
Matko Bulic <bulicmatko@gmail.com>
2018-05-14 18:49:53 +0200

    feat(messages-list): Add messages list scroll to last message functionality
    Add scroll to last message functionality.

--- src/components/messages-list.component.jsx
+++ src/components/messages-list.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { CSSTransitionGroup } from 'react-transition-group'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
@@ -6,56 +6,66 @@ import classNames from 'classnames'
 /**
  *  Messages List
  */
-const MessagesList = ({ activeUser, messages }) => (
-  <ul className="messages-list">
-    {messages.map(message => (
-      <li
-        key={message.id}
-        className={classNames(`message message--${message.type}`, {
-          'message--my-message': message.userId === activeUser.id,
-        })}
-      >
-        <CSSTransitionGroup
-          transitionName={
-            message.userId === activeUser.id
-              ? 'slide-in-from-right'
-              : 'slide-in-from-left'
-          }
-          transitionAppear={true}
-          transitionAppearTimeout={150}
-          transitionEnterTimeout={150}
-          transitionLeaveTimeout={150}
-        >
-          <span className="message-text">
-            {message.text}
-          </span>
-        </CSSTransitionGroup>
-      </li>
-    ))}
-  </ul>
-)
-
-/**
- *  Define Messages List Prop Types
- */
-MessagesList.propTypes = {
-  activeUser: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-  }).isRequired,
-  messages: PropTypes.arrayOf(
-    PropTypes.shape({
+export default class extends PureComponent {
+  static propTypes = {
+    activeUser: PropTypes.shape({
       id: PropTypes.number.isRequired,
-      type: PropTypes.oneOf([
-        'normal',
-        'thinking',
-      ]).isRequired,
-      text: PropTypes.string.isRequired,
-    }),
-  ).isRequired
-}
+      name: PropTypes.string.isRequired,
+    }).isRequired,
+    messages: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        type: PropTypes.oneOf([
+          'normal',
+          'thinking',
+        ]).isRequired,
+        text: PropTypes.string.isRequired,
+      }),
+    ).isRequired
+  }
 
-/**
- *  Export Messages List
- */
-export default MessagesList
+  componentDidMount () {
+    this.handleScrollToLastMessage()
+  }
+
+  componentDidUpdate () {
+    this.handleScrollToLastMessage()
+  }
+
+  handleScrollToLastMessage = () => (
+    this.listEl.scrollTop = this.listEl.scrollHeight
+  )
+
+  render () {
+    const { activeUser, messages } = this.props
+
+    return (
+      <ul className="messages-list" ref={component => this.listEl = component}>
+        {messages.map(message => (
+          <li
+            key={message.id}
+            className={classNames(`message message--${message.type}`, {
+              'message--my-message': message.userId === activeUser.id,
+            })}
+          >
+            <CSSTransitionGroup
+              transitionName={
+                message.userId === activeUser.id
+                  ? 'slide-in-from-right'
+                  : 'slide-in-from-left'
+              }
+              transitionAppear={true}
+              transitionAppearTimeout={150}
+              transitionEnterTimeout={150}
+              transitionLeaveTimeout={150}
+            >
+              <span className="message-text">
+                {message.text}
+              </span>
+            </CSSTransitionGroup>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+}
