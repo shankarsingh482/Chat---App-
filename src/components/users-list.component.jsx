f6f140432da60c13a99a07f476f30101d6c40b9e
Matko Bulic <bulicmatko@gmail.com>
2018-05-14 17:27:32 +0200

    feat(users-list): Add user highlighting
    Highlight currently active user.

--- src/components/users-list.component.jsx
+++ src/components/users-list.component.jsx
@@ -4,10 +4,15 @@ import PropTypes from 'prop-types'
 /**
  *  Users List
  */
-const UsersList = ({ users }) => (
+const UsersList = ({ activeUser, users }) => (
   <ul className="users-list">
     {users.map(user => (
-      <li key={user.id}>{user.name}</li>
+      <li key={user.id} className="user">
+        {user.id === activeUser.id
+          ? <strong>{user.name}</strong>
+          : <span>{user.name}</span>
+        }
+      </li>
     ))}
   </ul>
 )
@@ -16,6 +21,10 @@ const UsersList = ({ users }) => (
  *  Define Users List Prop Types
  */
 UsersList.propTypes = {
+  activeUser: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
   users: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
--- src/containers/users-list.container.js
+++ src/containers/users-list.container.js
@@ -4,6 +4,7 @@ import UsersList from '../components/users-list.component'
 
 export default connect(
   state => ({
+    activeUser: state.user,
     users: Object.keys(state.users).map(key => state.users[key]),
   }),
 )(UsersList)
