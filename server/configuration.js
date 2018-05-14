25824fa2e6ea297346a79a7babd0c929357a806f
Matko Bulic <bulicmatko@gmail.com>
2018-05-14 13:07:32 +0200

    feat(socket-server): Setup socket server
    Setup socket server with configurable port. Install dotenv package for environment variables
    handling.

---
+++ server/configuration.js
@@ -0,0 +1,5 @@
+require('dotenv').config()
+
+module.exports = {
+  SOCKET_SERVER_PORT: process.env.REACT_APP_SOCKET_SERVER_PORT,
+}
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const WebSocket = require('ws')
+
+const { SOCKET_SERVER_PORT } = require('./configuration')
+
+const server = new WebSocket.Server({ port: SOCKET_SERVER_PORT })
+
+server.on('connection', ws => {
+  console.log('Connection opened...')
+
+  ws.on('close', () => {
+    console.log('Connection closed...')
+  })
+})
+
+console.log(`Socket server running on port ${SOCKET_SERVER_PORT}`)
