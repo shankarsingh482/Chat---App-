c339341bbdc86af0008d6942e23bcd12c6c3a3c5
Matko Bulic <bulicmatko@gmail.com>
2018-05-14 11:59:19 +0200

    test(application): Fix tests by passing store to application component
    Pass store to application component and change extension of application test file.

--- src/application.test.jsx
+++ src/application.test.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import store from './store'
 import Application from './application'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<Application />, div)
+  ReactDOM.render(<Application store={store} />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
