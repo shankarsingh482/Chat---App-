2ef7ca73e5f3e2e132b3a08d2e4a6ac66c37362b
Matko Bulic <bulicmatko@gmail.com>
2018-05-14 13:35:05 +0200

    feat(socket-server): Broadcast when new user is connected
    Setup socket server to broadcast message to all open socket about new user connection.

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const server = new WebSocket.Server({ port: SOCKET_SERVER_PORT })
 server.on('connection', ws => {
   console.log('Connection opened...')
 
+  server.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify({ message: 'NEW USER CONNECTED' }))
+    }
+  })
+
   ws.on('message', message => {
     console.log(message)
   })
