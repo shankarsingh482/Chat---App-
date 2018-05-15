44f7261a926b11794fedb47bd6562380de844d8c
Matko Bulic <bulicmatko@gmail.com>
2018-05-15 17:45:22 +0200

    feat(chat): Add smiley and winking emoji
    Add super cool helper function that adds emoji to message text. :)

--- src/middleware/message-enricher.middleware.js
+++ src/middleware/message-enricher.middleware.js
@@ -1,5 +1,12 @@
 import { SET_USER_TYPING, CREATE_NEW_MESSAGE } from '../constants/internal-action-types.const'
 
+// pun intended :D
+const emojizz = text => (
+  text
+    .replace(':)', '\u{1F642}')
+    .replace(';)', '\u{1F609}')
+)
+
 export default store => next => action => {
   if (action.type === CREATE_NEW_MESSAGE) {
     const activeUser = store.getState().user
@@ -8,6 +15,7 @@ export default store => next => action => {
       ...action,
       payload: {
         ...action.payload,
+        text: emojizz(action.payload.text),
         userId: activeUser.id,
       },
     })
